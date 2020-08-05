fa947ac6cc77536aaf7b97de493f2a6e3a5826d7
Elizabeth King <elizabethking@improbable.io>
2020-08-05 13:35:48 +0100

    Add dotnet-format hook and sample files for testing

---
+++ samples/Testing.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test
+{
+	public class Testing
+	{
+	        public void DoSomething()
+	        {
+	            Console.WriteLine("Do something");
+		}								            }
+	}
+}
+
